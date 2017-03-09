7d9b31229dd8fca6a21aa7f4281ec510695ac1a8
dkaczor97 <dawidkaczor97@gmail.com>
2017-03-09 20:17:05 +0100

    a

---
+++ Polsl Invaders/Assets/scripts/player_movement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player_movement : MonoBehaviour {
+
+    float maxSpeed = 10;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+       
+
+        transform.Translate(new Vector3(0, Input.GetAxis("Horizontal") * maxSpeed * Time.deltaTime, 0));
+	
+	}
+}
