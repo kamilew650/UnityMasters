15a1173ede8a128b5153d51cbd0747f4295edf12
kamil <kamil.lewandowski1324@gmail.com>
2017-03-09 21:28:41 +0100

    d

---
+++ Polsl Invaders/Assets/scripts/StartField_Create.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class StartField_Create : MonoBehaviour {
+
+    
+	
+
+
+    // Use this for initialization
+	void Start () {
+
+        CreateEnemy();
+
+    }
+
+    public static void CreateEnemy()
+    {
+        GameObject enemy = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+        enemy.GetComponent<Transform>().position = (new Vector3(0, 5, -2.45f));
+
+        enemy.GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
