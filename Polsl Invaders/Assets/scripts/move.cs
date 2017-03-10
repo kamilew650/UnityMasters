2c9d26e8a0a35bfe7b980168a91078ad830be158
dkaczor97 <dawidkaczor97@gmail.com>
2017-03-10 16:06:40 +0100

    szczelanie

---
+++ Polsl Invaders/Assets/scripts/move.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour {
+
+    public float maxSpeed=10;
+	// Use this for initialization
+	void Start () {
+
+	
+	}
+
+    void Update()
+    {
+        transform.Translate(new Vector3(0,maxSpeed * Time.deltaTime, 0));
+    }
+	
+	
+}
---
+++ Polsl Invaders/Assets/scripts/playerShooting.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerShooting : MonoBehaviour {
+
+    public GameObject bulletPrefab;
+
+    public float fireDelay = 0.5f;
+    float coolDownTimer = 0;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        coolDownTimer -= Time.deltaTime;
+
+        if(Input.GetButton("Fire1") && coolDownTimer<=0)
+        {
+            coolDownTimer = fireDelay;
+
+            Instantiate(bulletPrefab,new Vector3(transform.position.x,transform.position.y,transform.position.z));
+        }
+	
+	}
+}
