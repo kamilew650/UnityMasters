c9f2d416f563bda2479f2ff20ce4ef78f284775d
kamil <kamil.lewandowski1324@gmail.com>
2017-03-15 13:10:00 +0100

    no to mamy bosa

--- Polsl Invaders/Assets/scripts/GameController.cs
+++ Polsl Invaders/Assets/scripts/GameController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GameController : MonoBehaviour {
 
     public GameObject enemy;
+    public GameObject boss;
     public Vector3 spawnValues;
     public int enemyCount;              //ilosc przeciwnikow
     public float spawnWait;             //co ile sie maja pojawiac
@@ -18,13 +19,16 @@ public class GameController : MonoBehaviour {
     IEnumerator spawnWaves() //ni mom pojecia czemu funkcja ma taki typ
     {
         yield return new WaitForSeconds(spawnStart);
-        for (int i = 0; i < enemyCount; i++)
+        int i;
+        Quaternion spawnRotation = Quaternion.identity;
+        for ( i = 0; i < enemyCount; i++)
         {
             Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
-            Quaternion spawnRotation = Quaternion.identity;
             Instantiate(enemy, spawnPosition, spawnRotation);
             yield return new WaitForSeconds(spawnWait);
         }
+        
+            Instantiate(boss, new Vector3(0, spawnValues.y, spawnValues.z),  spawnRotation);
     }
 	
 	// Update is called once per frame
